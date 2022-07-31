aadb8588a56fdd5f85a043153fb5eea01feb0460
NgoTanKhoa21042001 <Kakashi2104@gmail.com>
2022-07-31 22:17:18 +0700

    Up load source code

--- App.js
+++ App.js
@@ -1,10 +1,10 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { StyleSheet, Text, View, StatusBar } from "react-native";
+import Constants from "expo-constants";
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View>
+      <StatusBar style="dark" backgroundColor="#61dafb" />
+      <Text>Open up App.js to start working on your p!</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +13,9 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+    paddingTop: Constants.statusBarHeight,
   },
 });
